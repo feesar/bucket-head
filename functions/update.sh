04f869a63f90f543c5e1dba268338dc5bcc72877
TheTabKey <tyler@tylerbloom.net>
2022-07-11 17:12:25 -0400

    oxide check fix

--- functions/update.sh
+++ functions/update.sh
@@ -10,50 +10,46 @@ writeLog "update" "Checking for server update"
 
 if [[ $(serverManager "cu") == *"Update available"* ]]
 then
-  if isMonthlyUpdate && [ "$umod_update" = true ]
+  if isMonthlyUpdate && [ "$umod_update" = true ] && [[ $(oxidecheck) != $(date +%F) ]]
   then
-    if [[ $(oxidecheck) != $(date +%F) ]]
-    then
-      writeLog "update" "waiting for oxide update"
-      return 0
-    fi
-  else
-    writeLog "update" "Doing server update"
-
-    writeLog "update" "Stopping server"
-    serverManager "stop"
-
-    writeLog "update" "Updating server"
-    serverManager "u"
-
-    if [ "$umod_update" = true ]
-    then
-      if [ "$umod_backup_groups" = true ]
-      then
-        writeLog "update" "Backup Umod groups from oxide"
-        mkdir $BASEDIR/tmp
-        mv $executable_path/serverfiles/oxide/data/oxide.groups.data $BASEDIR/tmp
-      fi
-
-      writeLog "update" "Updating Umod"
-      serverManager "mu"
-
-      if [ "$umod_backup_groups" = true ]
-      then
-        writeLog "update" "Restoring Umod groups from temp"
-        mv $BASEDIR/tmp/oxide.groups.data $executable_path/serverfiles/oxide/data/
-        rm -rf $BASEDIR/tmp
-      fi
-    fi
-
-    if isMonthlyUpdate
-    then
-      . $BASEDIR/functions/wipe.sh "devblog"
-      return 0
-    fi
-
-    writeLog "update" "Starting server"
-    serverManager "start"
+    writeLog "update" "waiting for oxide update"
+    return 0
   fi
+  writeLog "update" "Doing server update"
+
+  writeLog "update" "Stopping server"
+  serverManager "stop"
+
+  writeLog "update" "Updating server"
+  serverManager "u"
+
+  if [ "$umod_update" = true ]
+  then
+    if [ "$umod_backup_groups" = true ]
+    then
+      writeLog "update" "Backup Umod groups from oxide"
+      mkdir $BASEDIR/tmp
+      mv $executable_path/serverfiles/oxide/data/oxide.groups.data $BASEDIR/tmp
+    fi
+
+    writeLog "update" "Updating Umod"
+    serverManager "mu"
+
+    if [ "$umod_backup_groups" = true ]
+    then
+      writeLog "update" "Restoring Umod groups from temp"
+      mv $BASEDIR/tmp/oxide.groups.data $executable_path/serverfiles/oxide/data/
+      rm -rf $BASEDIR/tmp
+    fi
+  fi
+
+  if isMonthlyUpdate
+  then
+    . $BASEDIR/functions/wipe.sh "devblog"
+    return 0
+  fi
+
+  writeLog "update" "Starting server"
+  serverManager "start"
 
 fi
